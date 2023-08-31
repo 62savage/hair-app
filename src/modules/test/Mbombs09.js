ee4553d689f0797c901302c6e75d8f23a4cb3921
gwangbaekun <gwangbaekun@gmail.com>
2023-08-31 16:05:24 +0900

    mock data

--- src/modules/test/Mbombs09.js
+++ src/modules/test/Mbombs09.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { colors } from '../../styles';
+import { StyleSheet, View } from 'react-native';
+
 import CustomButton from '../../components/Button';
+import { Text } from '../../components/StyledText';
+
+import { colors } from '../../styles';
 
 export default function Mbombs09(props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.textAboveButton}>
-        최근 1년 이내 {'\n'}셀프 염색 혹은 탈색{'\n'} 시술한 경험이 있다.
+      <Text hCenter size={22}>
+        최근 1년 이내 {'\n'}셀프 염색 혹은 탈색{'\n'}시술한 경험이 있다.
       </Text>
       <CustomButton>
         <Text style={styles.buttonText}>YES</Text>
@@ -28,14 +31,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     gap: 10,
   },
-  textAboveButton: {
-    width: '100%',
-    color: '#fff',
-    fontSize: 24,
-    width: 210,
-    textAlign: 'center',
-    marginBottom: 25,
-  },
   buttonText: {
     fontSize: 12,
     color: '#221F32',
@@ -44,3 +39,58 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
+/**
+ *
+ * @todo
+ * - [ ] change to scroll view
+ * - [ ] extract button to component
+ * - [ ] with the button component, change it to module
+ */
+
+const apiRequestUrl =
+  'http://ec2-user@ec2-43-201-111-38.ap-northeast-2.compute.amazonaws.com:8080/node/478';
+const apiRequestUrl2 =
+  'http://ec2-user@ec2-43-201-111-38.ap-northeast-2.compute.amazonaws.com:8080/node/478';
+
+const mockData = {
+  ID: 478,
+  CreatedAt: '2023-07-14T06:51:37.831032Z',
+  UpdatedAt: '2023-07-14T07:02:20.850465Z',
+  DeletedAt: null,
+  Name: '당신의 퍼스널 컬러는?\t',
+  Deps: 0,
+  ParentID: null,
+  Parent: null,
+  ChildrenIDs: [
+    {
+      imgUrl: '',
+      name: '봄웜',
+      id: 480,
+    },
+    {
+      imgUrl: '',
+      name: '가을웜\t\t',
+      id: 481,
+    },
+    {
+      imgUrl: '',
+      name: '여름쿨\t\t',
+      id: 482,
+    },
+    {
+      imgUrl: '',
+      name: '겨울쿨\t\t',
+      id: 483,
+    },
+  ],
+  Position: {
+    X: 472,
+    Y: -174,
+  },
+  ImgUrl: '',
+  IsFinal: false,
+  Grouping: 0,
+  Category: 2,
+  NextCategory: 3,
+};
--- src/modules/test/TestViewContainer.js
+++ src/modules/test/TestViewContainer.js
@@ -2,5 +2,6 @@ import { connect } from 'react-redux';
 import { compose, lifecycle } from 'recompose';
 
 import TestScreen from './TestView';
+import Mbombs09 from './Mbombs09';
 
-export default compose()(TestScreen);
+export default compose()(Mbombs09);
